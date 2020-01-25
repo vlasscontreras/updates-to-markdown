b6e7203c4946c331341e98e5fa47b9586abbd4e9
Vladimir Contreras <17748144+vlasscontreras@users.noreply.github.com>
2020-01-25 13:44:19 -0600

    Update build

---
+++ precache-manifest.c6c7748af00b004037624f89845889c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9aa2b555219b0c846b3f436b1c51043",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9ba5b286971dc12337da",
+    "url": "./static/css/main.777357c7.chunk.css"
+  },
+  {
+    "revision": "2717d53ecc10db692648",
+    "url": "./static/js/2.415a1b62.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.415a1b62.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9ba5b286971dc12337da",
+    "url": "./static/js/main.2011a101.chunk.js"
+  },
+  {
+    "revision": "0d330dd9bd884aa089e1",
+    "url": "./static/js/runtime-main.98f9cdfa.js"
+  }
+]);
