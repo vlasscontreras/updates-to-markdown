dc298d41addc3c8148ba21716931e12931a3c45d
Vladimir Contreras <17748144+vlasscontreras@users.noreply.github.com>
2020-01-28 16:58:29 -0600

    Update build

--- build/precache-manifest.eb3ca01be4356c2b783a91cecab5e31e.js
+++ build/precache-manifest.eb3ca01be4356c2b783a91cecab5e31e.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fc375ceac0e58bcf602c9cbaeba1bf0f",
+    "revision": "3efce2596fce09460959d4ca65b97518",
     "url": "./index.html"
   },
   {
-    "revision": "9ba5b286971dc12337da",
+    "revision": "54a5840db8dea6b63084",
     "url": "./static/css/main.777357c7.chunk.css"
   },
   {
-    "revision": "2717d53ecc10db692648",
-    "url": "./static/js/2.415a1b62.chunk.js"
+    "revision": "07277b51c5d5a9a4cc3c",
+    "url": "./static/js/2.487e0410.chunk.js"
   },
   {
     "revision": "d705cb622423d72c5defbf368ca70dcc",
-    "url": "./static/js/2.415a1b62.chunk.js.LICENSE"
+    "url": "./static/js/2.487e0410.chunk.js.LICENSE"
   },
   {
-    "revision": "9ba5b286971dc12337da",
-    "url": "./static/js/main.2011a101.chunk.js"
+    "revision": "54a5840db8dea6b63084",
+    "url": "./static/js/main.aa50b034.chunk.js"
   },
   {
     "revision": "0d330dd9bd884aa089e1",
